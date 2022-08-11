15ee4eb2557890645f7f53cab26d8a283c22da69
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-08-11 15:53:53 +0400

    fix: countdown was not stopping at 0

--- src/useCountDown.js
+++ src/useCountDown.js
@@ -1,16 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+const countTimeInMs = (minutes, seconds) => minutes * 60 * 1000 + seconds * 1000
 
 const useCountDown = timeOutFn => {
   const [minutes, setMinutes] = useState(0)
   const [seconds, setSeconds] = useState(0)
   const [timerID, setTimerID] = useState(0)
-  const timeInMS = minutes * 60 * 1000 + seconds * 1000
+  const [timeInMS, setTimeInMS] = useState(0)
 
   const clearCountDown = () => {
     clearInterval(timerID)
-    setMinutes(0)
-    setSeconds(0)
+    setMinute(0)
+    setSecond(0)
   }
+
+  useEffect(() => {
+    setTimeInMS(countTimeInMs(minutes, seconds))
+  }, [minutes, seconds])
 
   const setMinute = newMin => {
     setMinutes(newMin)
@@ -20,37 +26,36 @@ const useCountDown = timeOutFn => {
   }
 
   const startCountDown = () => {
-    if (!timerID) {
+    if (!timerID && (minutes > 0 || seconds > 0)) {
       const id = setInterval(displayMinutesAndSeconds, 1000)
       setTimerID(id)
     }
-  }
+    console.log(timerID)
 
-  setTimeout(() => {
-    clearInterval(timerID)
-    timeOutFn()
-  }, timeInMS)
+    setTimeout(() => {
+      clearCountDown()
+      timeOutFn()
+    }, timeInMS)
+  }
 
   let counter = 0
   const displayMinutesAndSeconds = () => {
     counter = counter + 1
     const newMs = timeInMS - 1000 * counter
+    if (newMs > 0) {
+      const newMin = Math.floor(newMs / 1000 / 60)
+      const newSec = Math.floor((newMs / 1000) % 60)
 
-    const newMin = Math.floor(newMs / 1000 / 60)
-    const newSec = Math.floor((newMs / 1000) % 60)
-
-    setMinutes(newMin)
-    setSeconds(newSec)
+      setMinute(newMin)
+      setSecond(newSec)
+    } else {
+      clearInterval(timerID)
+      setMinute(0)
+      setSecond(0)
+    }
   }
 
-  return [
-    minutes,
-    setMinute,
-    seconds,
-    setSecond,
-    startCountDown,
-    clearCountDown,
-  ]
+  return [minutes, setMinute, seconds, setSecond, startCountDown, timerID]
 }
 
 export default useCountDown
