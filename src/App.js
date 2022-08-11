5295876665f21edafecfaff8ec6e4452e153687e
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-08-11 16:14:17 +0400

    add input to the app and use custom hook to start timer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useState } from 'react'
+import useCountDown from './useCountDown'
+
+const timeOutFn = () => {
+  console.log('Timer Expired')
 }
 
-export default App;
+function App() {
+  const [isTimerOn, setIsTimerOn] = useState(false)
+  const [minutes, setMinutes, seconds, setSeconds, startCountDown, timerID] =
+    useCountDown(timeOutFn)
+
+  const clearCountDown = () => {
+    clearInterval(timerID)
+    setMinutes(0)
+    setSeconds(0)
+  }
+  const startStopTimer = () => {
+    if (!isTimerOn) {
+      startCountDown()
+    }
+    if (isTimerOn) {
+      clearCountDown()
+    }
+    setIsTimerOn(!isTimerOn)
+  }
+  return (
+    <div>
+      {!isTimerOn ? (
+        <div>
+          <input
+            type='number'
+            value={minutes}
+            onChange={e => setMinutes(e.target.value)}
+            min={0}
+            max={15}
+            maxLength={2}
+          ></input>
+          :
+          <input
+            type='number'
+            value={seconds}
+            onChange={e => setSeconds(e.target.value)}
+            min={0}
+            max={50}
+            maxLength={2}
+          ></input>
+        </div>
+      ) : (
+        <div>
+          <label>{minutes}</label>:<label>{seconds}</label>
+        </div>
+      )}
+      <button onClick={startStopTimer}>
+        {isTimerOn ? 'Reset/Stop Timer' : 'Start Timer'}
+      </button>
+    </div>
+  )
+}
+
+export default App
